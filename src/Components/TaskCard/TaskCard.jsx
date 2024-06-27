f57a443df44ba37375aa6d71ae62d8dc0777a306
ayoncoding404 <muhayon.rxz@gmail.com>
2024-06-27 18:03:18 +0600

    apply right-click context menu

--- src/Components/TaskCard/TaskCard.jsx
+++ src/Components/TaskCard/TaskCard.jsx
@@ -1,20 +1,21 @@
-import styles from './TaskCard.module.css'
+import styles from "./TaskCard.module.css";
 import { GoTrash } from "react-icons/go";
 
-const TaskCard = ({title, description, handleDelete, index}) => {
+const TaskCard = ({ title, description, handleDelete, index }) => {
   return (
     <article className={styles.task_card}>
-        <p className={styles.task_text}>
-             {title}
-        </p>
-        <div className={styles.task_card_bottom}>
-              <div className={styles.task_description}>{description}</div>
-              <button onClick={()=>handleDelete(index)} className={styles.task_delete}>
-              <GoTrash />
-              </button>  
-        </div>
+      <p className={styles.task_text}>{title}</p>
+      <div className={styles.task_card_bottom}>
+        <div className={styles.task_description}>{description}</div>
+        <button
+          onClick={() => handleDelete(index)}
+          className={styles.task_delete}
+        >
+          <GoTrash />
+        </button>
+      </div>
     </article>
-  )
-}
+  );
+};
 
-export default TaskCard
+export default TaskCard;
